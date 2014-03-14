659e4bd3734ff034f5b6fc22a9bd0711b6aa97d3
Didrik Pinte <dpinte@enthought.com>
2014-03-14 16:52:54 +0100

    moving ui code out of the hydropick.model.algorithms

---
+++ hydropick/ui/algorithm_presenter.py
@@ -0,0 +1,28 @@
+""" Algorithm presenters. """
+from traits.api import HasStrictTraits, Instance
+from traitsui.api import View, Item, TextEditor, Group, UItem, Label
+
+from hydropick.model.i_algorithm import IAlgorithm
+
+class AlgorithmPresenter(HasStrictTraits):
+
+    algorithm = Instance(IAlgorithm)
+
+    def traits_view(self):
+        view = View(
+            Group(
+                Label('Instructions:', emphasized=True),
+                UItem('object.algorithm.instructions', editor=TextEditor(), style='readonly',
+                      emphasized=True)
+            ),
+            Item('_'),
+            *['object.algorithm.{}'.format(arg) for arg in self.algorithm.arglist],
+            buttons=['OK', 'Cancel'],
+            kind='modal'
+        )
+        return view
+
+if __name__ == '__main__':
+    from hydropick.model.algorithms import XDepthAlgorithm
+    presenter = AlgorithmPresenter(algorithm=XDepthAlgorithm())
+    presenter.configure_traits()
